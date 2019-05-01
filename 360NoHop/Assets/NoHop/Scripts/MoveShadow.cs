5cd0f1102886913dd4c07108b4ca12f12d331c64
BeardedAngel <oded56@gmail.com>
2019-05-01 22:57:51 +0300

    Sorta working platform auto rotation

---
+++ 360NoHop/Assets/NoHop/Scripts/MoveShadow.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveShadow : MonoBehaviour
+{
+    [SerializeField] Transform headPosision;
+
+
+    private void Update()
+    {
+        headPosision = Camera.main.transform;
+        transform.GetComponent<Collider>().transform.SetPositionAndRotation(transform.position, headPosision.rotation);  
+    }
+}
